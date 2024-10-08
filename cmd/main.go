13ef59a4594a363abaf9be3ba1ef8d28f30a7648
Noah CHARRIN--BOURRAT <noah.charrinbourrat@ynov.com>
2024-10-08 22:32:33 +0200

    readfile func has been added

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"fmt"
+	"hangman-classic/internal/game"
+)
+
 func main() {
-	return
+
+	game.ShowHangman([]rune("         \n         \n         \n         \n         \n         \n=========\n\n         \n      |  \n      |  \n      |  \n      |  \n      |  \n=========\n\n  +---+  \n      |  \n      |  \n      |  \n      |  \n      |  \n=========\n\n  +---+  \n  |   |  \n      |  \n      |  \n      |  \n      |  \n=========\n\n  +---+  \n  |   |  \n  O   |  \n      |  \n      |  \n      |  \n=========\n\n  +---+  \n  |   |  \n  O   |  \n  |   |  \n      |  \n      |  \n=========\n\n  +---+  \n  |   |  \n  O   |  \n /|   |  \n      |  \n      |  \n=========\n\n  +---+  \n  |   |  \n  O   |  \n /|\\  |  \n      |  \n      |  \n=========\n\n  +---+  \n  |   |  \n  O   |  \n /|\\  |  \n /    |  \n      |  \n=========\n\n  +---+  \n  |   |  \n  O   |  \n /|\\  |  \n / \\  |  \n      |  \n=========\n\n"), 1)
+	fmt.Println(game.ReadFile("data/words.txt"))
+	test := []rune{98, 97, 110, 99, 10, 98, 117, 114, 101, 97, 117, 10, 99, 97, 98, 105, 110, 101, 116, 10, 99, 97, 114, 114, 101, 97, 117, 10}
+	fmt.Print(game.RandomWord(test))
 }
---
+++ internal/game/game.go
@@ -0,0 +1,57 @@
+package game
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+)
+
+type HangManData struct {
+	Word             string     // Word composed of '_', ex: H_ll_
+	ToFind           string     // Final word chosen by the program at the beginning. It is the word to find
+	Attempts         int        // Number of attempts left
+	HangmanPositions [10]string // It can be the array where the positions parsed in "hangman.txt" are stored
+}
+
+func Init() {
+	var data HangManData
+	//data.ToFind = RandomWord("hangman-classic/data/word.txt")
+	//data.Word = len(data.ToFind)
+	data.Attempts = 10
+}
+
+func ReadFile(nameFile string) []byte {
+	fileContent, err := ioutil.ReadFile(nameFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Convert []byte to string
+	return fileContent
+}
+
+func ShowHangman(hangman []rune, attempts int) [7][9]string {
+	var tab [7][9]string
+	//var tabReturn [10]string
+	for i := 0; i < 7; i++ {
+		for j := 0; j < 9; j++ {
+			if rune(hangman[i*j]) != 10 {
+				tab[i][j] = string(hangman[i*j])
+			}
+		}
+	}
+	fmt.Println(tab)
+	return tab
+}
+
+func RandomWord(wordFile []rune) [][]string {
+	var wordTab [][]string
+	compter := 0
+	for i := 0; i < len(wordFile); i++ {
+		if rune(wordFile[i]) != 10 {
+			wordTab = append(wordTab, wordFile[i])
+		} else {
+			compter++
+		}
+	}
+	return wordTab
+}
