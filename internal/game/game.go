e8c38230ba7cb8450a43d302fa3fddfdbcb881d5
cnoah <noah.charrinbourrat@ynov.com>
2024-10-12 17:16:44 +0200

    bug fixes and merge of ReadFile and TransformTab functions

--- internal/game/game.go
+++ internal/game/game.go
@@ -15,20 +15,16 @@ type HangManData struct {
 	Word             string     // Word composed of '_', ex: H_ll_
 	ToFind           string     // Final word chosen by the program at the beginning. It is the word to find
 	Attempts         int        // Number of attempts left
-	HangmanPositions [10]string // It can be the array where the positions parsed in "hangman.txt" are stored
+	HangmanPositions [][]string // It can be the array where the positions parsed in "hangman.txt" are stored
 }
 
-func ReadFile(nameFile string) []rune {
-	fileContent, err := ioutil.ReadFile(nameFile)
+func ReadFile(nameFile string) [][]string {
+	content, err := ioutil.ReadFile(nameFile)
 	if err != nil {
 		log.Fatal(err)
 	}
 	// Convert []byte to string
-	return []rune(string(fileContent))
-}
-
-func TransfomTab(wordFile []rune) [][]string {
-	// a integrer dans ReadFile
+	wordFile := []rune(string(content))
 	var wordTab [][]string
 	counter := 0
 	wordTab = append(wordTab, []string{})
@@ -45,34 +41,26 @@ func TransfomTab(wordFile []rune) [][]string {
 
 func RandomWord(list [][]string, data *HangManData) {
 	randomWordPos := rand.Intn(len(list))
-	for i := 0; i < len(list[randomWordPos]); i++ {
+	for i := 0; i < len(list[randomWordPos])-1; i++ {
 		data.Word += "_"
 		data.ToFind += list[randomWordPos][i]
 	}
 }
 
 func ShowHangman(hangman [][]string, attempts int) {
-	for i := 0 + attempts*7 + 1; i < 7+attempts*7; i++ {
-		if len(hangman[i]) == 0 {
-			for iBis := 0; iBis < 9; iBis++ {
-				fmt.Println("          ")
-			}
-			i++
-		}
-		for j := 0; j < len(hangman[i]); j++ {
-			if hangman[i][j] != "\n" {
-				fmt.Print(hangman[i][j])
-			}
+	hangmanReturn := hangman[attempts*7 : (attempts+1)*7]
+	for i := 0; i < len(hangmanReturn); i++ {
+		for j := 0; j < len(hangmanReturn[i]); j++ {
+			fmt.Print(hangmanReturn[i][j])
 		}
 		fmt.Print("\n")
 	}
-
 }
-
 func Init() {
 	var data HangManData
 	data.Attempts = 10
-	RandomWord(TransfomTab(ReadFile("data/words.txt")), &data)
+	RandomWord(ReadFile("data/words.txt"), &data)
+	data.HangmanPositions = ReadFile("data/hangman.txt")
 	Game(&data)
 }
 
@@ -82,7 +70,7 @@ func Game(data *HangManData) {
 		cmd := exec.Command("cmd", "/c", "cls")
 		cmd.Stdout = os.Stdout
 		cmd.Run()
-		//fmt.Println(ShowHangman(data.))
+		ShowHangman(data.HangmanPositions, data.Attempts)
 		fmt.Println(data.Word)
 		userInput := input.Input()
 		for i := 0; i < len(data.ToFind); i++ {
@@ -112,6 +100,6 @@ func Game(data *HangManData) {
 	if data.Attempts == 0 {
 		fmt.Println(data.Word + "\nYou loose :( \nYou've needed to find " + data.ToFind)
 	} else {
-		fmt.Println("You've won horray :D\nYou've needed to find " + data.ToFind)
+		fmt.Println("You've won horray :D\nYou've successfully found " + data.ToFind)
 	}
 }
