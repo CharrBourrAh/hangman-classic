cf19214e6460a41c4f7194c0b3fc2a3a8fb483a9
Noah CHARRIN--BOURRAT <noah.charrinbourrat@ynov.com>
2024-10-14 17:13:59 +0200

    input.go has been updated

--- internal/input/input.go
+++ internal/input/input.go
@@ -4,15 +4,15 @@ import "fmt"
 
 func Input() string {
 	var userInput string
-	authorizedCharacters := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ/"
+	forbiddenCharacters := "1234567890°+&é\"(-è_çà)'=^$*ù!:;,?.§%¨£µ€<>~#{[|`\\]}@"
 	fmt.Println("Enter an input :")
 	data, err := fmt.Scanf("%s \n", &userInput)
 	if err != nil || data != 1 {
 		return "Error"
 	}
 	for i := 0; i < len(userInput); i++ {
-		for j := 0; j < len(authorizedCharacters); j++ {
-			if userInput[i] != authorizedCharacters[j] {
+		for j := 0; j < len(forbiddenCharacters); j++ {
+			if userInput[i] == forbiddenCharacters[j] {
 				fmt.Println("Error")
 				return "Error"
 			}
