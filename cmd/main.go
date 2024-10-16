fb008c69ddf878a19af678145dd9d48dea65340c
cnoah <noah.charrinbourrat@ynov.com>
2024-10-17 00:57:42 +0200

    comments has been added to the main.go

--- cmd/main.go
+++ cmd/main.go
@@ -9,13 +9,16 @@ import (
 func main() {
 	if len(os.Args) > 1 {
 		if os.Args[1] == "words.txt" || os.Args[1] == "words2.txt" || os.Args[1] == "words3.txt" {
+			// If a words file is specified, the game will launch by using a random word for the given file
 			game.ClearCMD()
 			game.Init("data/" + os.Args[1])
 		} else if os.Args[1] == "--startWith" {
+			// If this argument is given, the game will resume the game thanks to the information on the save file given after
 			game.ClearCMD()
 			game.Resume(save.StartWithFlag("save/" + os.Args[2]))
 		}
 	} else {
+		// If no argument has been given, the game will boot the main menu
 		game.ClearCMD()
 		game.Menu()
 	}
